d0bf12e2883d929796ecd8c562fb11de1f5a7fb8
王立帮 <3294713004@qq.com>
2024-09-01 23:41:47 +0800

    Update: 调整默认文件路径
    调整文件package_index.tar.bz2和library_index.tar.bz2

--- internal/arduino/globals/globals.go
+++ internal/arduino/globals/globals.go
@@ -68,10 +68,10 @@ var (
 	}
 
 	// DefaultIndexURL is the default index url
-	DefaultIndexURL = "https://downloads.arduino.cc/packages/package_index.tar.bz2"
+	DefaultIndexURL = "http://download.mixlylibs.cloud/arduino-packages/default/package_index.tar.bz2"
 
 	// LibrariesIndexURL is the URL where to get the libraries index.
-	LibrariesIndexURL, _ = url.Parse("https://downloads.arduino.cc/libraries/library_index.tar.bz2")
+	LibrariesIndexURL, _ = url.Parse("http://download.mixlylibs.cloud/arduino-packages/default/library_index.tar.bz2")
 
 	// LibrariesIndexResource is the IndexResource to get the libraries index.
 	LibrariesIndexResource = resources.IndexResource{
